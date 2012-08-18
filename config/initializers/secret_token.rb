154bc183311f90944a9f56354025c50dcddd7cb5
Jeremy Corbett <jeremy.lael@gmail.com>
2012-08-18 19:04:37 -0400

    Checking in rails-api project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FunRapi::Application.config.secret_token = '481f73603c118540b03aabd1b515567a4bf8858a819027805788e092d17faf3cc8df0601803743fe172c5cc8d9c12c7aad93f7783ed140b9b0990dd8174fc2ad'
